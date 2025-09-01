bd6e9521d50c0a002d0615aa754e1ad402e0dd58
gopal <buragaddagopal345@gmail.com>
2025-09-01 17:43:58 +0530

    entity created

---
+++ src/main/java/com/employee/Entity/Employess_details.java
@@ -0,0 +1,14 @@
+package com.employee.Entity;
+
+
+public class Employess_details {
+	
+	private String empName;
+	private int empId;
+	private int empSal;
+	private String empAddress;
+	private int empNumber;
+	
+	
+
+}
