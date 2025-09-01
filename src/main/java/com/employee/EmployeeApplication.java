781a2312ee7c694b8775db175be524733f95af2f
gopal <buragaddagopal345@gmail.com>
2025-09-01 17:14:03 +0530

    removed some file

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -10,10 +10,7 @@ public class EmployeeApplication {
 		SpringApplication.run(EmployeeApplication.class, args);
 		
 		System.out.println("Employee Application Started..");
-		System.out.println("EmployeeApplication.main()");
 
-		System.out.println("Employee Application Started..");
-		System.out.println("EmployeeApplication.main()");
 	}
 	
 
