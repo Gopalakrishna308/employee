613fa651e40c3563e0d975af3ef03045cd608e81
gopal <buragaddagopal345@gmail.com>
2025-11-28 12:46:42 +0530

    line add

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService = new EmployeeService();
+
 
 	@PostMapping("/save")
 	public String saveEmploye(@RequestBody Employee employee) {
